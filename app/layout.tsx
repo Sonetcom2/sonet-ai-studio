ab423058f0e2efd069a332d99826eb3b0c13d567
Sonetcom2 <sonetcom26@gmail.com>
2026-08-23 19:34:12 +0100

    Add Meta Pixel tracking

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import "./globals.css";
 import { AuthProvider } from "@/components/providers/AuthProvider";
 import type { Metadata, Viewport } from "next";
+import Script from "next/script";
 
 export const metadata: Metadata = {
   title: "SONET AI STUDIO",
@@ -21,6 +22,30 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
+        <Script
+          id="meta-pixel"
+          strategy="afterInteractive"
+        >{`
+          !function(f,b,e,v,n,t,s)
+          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+          if(!f._fbq)f._fbq=n;
+          n.push=n;
+          n.loaded=!0;
+          n.version='2.0';
+          n.queue=[];
+          t=b.createElement(e);
+          t.async=!0;
+          t.src=v;
+          s=b.getElementsByTagName(e)[0];
+          s.parentNode.insertBefore(t,s)
+          }(window, document,'script',
+          'https://connect.facebook.net/en_US/fbevents.js');
+
+          fbq('init', '1455599342937490');
+          fbq('track', 'PageView');
+        `}</Script>
+
         <AuthProvider>
           {children}
         </AuthProvider>
